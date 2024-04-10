90230722c8c83d42438677a9403098797df0c4b3
Tony <guzantony@gmail.com>
2024-04-10 21:47:24 +0300

    ssh-key added

--- vars.tf
+++ vars.tf
@@ -10,3 +10,12 @@ variable "droplet_count" {
 variable "name" {
   default = "Fourth_Estate"
 }
+
+variable "image" {
+  default = "ubuntu-20-04-x64"
+}
+
+variable "droplet_size" {
+  default = "s-1vcpu-1gb"
+}
+
--- web-servers.tf
+++ web-servers.tf
@@ -1,13 +1,18 @@
 resource "digitalocean_droplet" "web" {
   count = var.droplet_count
   image = var.image
-  name = "web-${var.name}-${count.index + 1}"
+  name = "web-${count.index + 1}"
   region = var.region
   size = var.droplet_size
-  ssh_keys = //
+  ssh_keys = [digitalocean_ssh_key.default.fingerprint]
   vpc_uuid = digitalocean_vpc.project.id
   tags = ["${var.name}-webserver"]
   lifecycle {
     create_before_destroy = true
   }  
+  
+}
+resource "digitalocean_ssh_key" "default" {
+  name       = "Terraform Example"
+  public_key = file("D:/System/Users/User-Arch/.ssh/id_rsa.pub")
 }
