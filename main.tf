2c1f6500ae02f208171831d2bdc89af183d35ecb
Tony <guzantony@gmail.com>
2024-05-14 17:21:48 +0300

    send inventory file to bastion

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "local_file" "inventory" {
   filename = "${path.module}/Ansible/inventory.txt"
   content  = data.template_file.inventory_template.rendered
 }
+
+resource "null_resource" "send_inventory_to_bastion" {
+  depends_on = [local_file.inventory]
+
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no ${path.module}/Ansible/inventory.txt root@${digitalocean_droplet.bastion.ipv4_address}:${path.module}/Ansible/inventory.txt"
+  }
+}
