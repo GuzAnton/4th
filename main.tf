f3cb1c1a4e3aa41829e321c65a6e5eba02e2b105
Tony <guzantony@gmail.com>
2024-05-14 17:37:56 +0300

    modified scp inventory.txt

--- main.tf
+++ main.tf
@@ -11,6 +11,14 @@ resource "null_resource" "send_inventory_to_bastion" {
   depends_on = [local_file.inventory]
 
   provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no ${path.module}/Ansible/inventory.txt root@${digitalocean_droplet.bastion.ipv4_address}:${path.module}/4th/Ansible/inventory.txt"
+    command = <<-EOT
+      retries=2
+      count=0
+      while [ $count -lt $retries ]; do
+        scp -o StrictHostKeyChecking=no ${path.module}/Ansible/inventory.txt root@${digitalocean_droplet.bastion.ipv4_address}:${path.module}/4th/Ansible/inventory.txt && break
+        count=$((count+1))
+        sleep 20
+      done
+    EOT
   }
 }
