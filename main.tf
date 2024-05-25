22fc6aac78c9823440cacece90a28a19071155dc
Tony <guzantony@gmail.com>
2024-05-25 12:37:42 +0300

    changes to main

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "digitalocean" {
   token = var.do_token
 }
-provider "cloudflare" {
-  api_token = var.cf_api_token
-}
+# provider "cloudflare" {
+#   api_token = var.cf_api_token
+# }
 
 resource "local_file" "inventory" {
   filename = "${path.module}/Ansible/inventory.txt"
