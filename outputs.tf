a944d2f8fa6937f9a39107c32201ff26a8d66fee
Tony <guzantony@gmail.com>
2024-06-04 14:31:37 +0300

    test frontend

--- outputs.tf
+++ outputs.tf
@@ -14,9 +14,9 @@ output "db_server_private_ips" {
 # output "bastion_private_ip" {
 #   value = digitalocean_droplet.bastion.ipv4_address_private
 # }
-# output "web_public_ip" {
-#   value = digitalocean_droplet.web.*.ipv4_address
-# }
+output "web_public_ip" {
+  value = digitalocean_droplet.web.*.ipv4_address
+}
 # output "db_public_ip" {
 #   value = digitalocean_droplet.db.*.ipv4_address
 # }
