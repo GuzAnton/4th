182988e249eceef70375219cc5f4da097637c7e9
Tony <guzantony@gmail.com>
2024-06-04 15:23:21 +0300

    test http

--- web-servers.tf
+++ web-servers.tf
@@ -85,6 +85,11 @@ resource "digitalocean_firewall" "web" {
     source_addresses = ["0.0.0.0/0"]
   }
   inbound_rule {
+    protocol = "tcp"
+    port_range = "80"
+    source_addresses = ["0.0.0.0/0"]
+  }
+  inbound_rule {
     protocol         = "tcp"
     port_range       = "1-65535"
     source_addresses = [digitalocean_vpc.project.ip_range]
