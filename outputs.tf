425deb228b5d0ceba3b884039a4809aa3af438e3
Tony <guzantony@gmail.com>
2024-06-04 14:54:21 +0300

    test

--- outputs.tf
+++ outputs.tf
@@ -10,13 +10,12 @@ output "web_servers_private_ips" {
 output "db_server_private_ips" {
   value = digitalocean_droplet.db.*.ipv4_address_private
 }
-
-# output "bastion_private_ip" {
-#   value = digitalocean_droplet.bastion.ipv4_address_private
-# }
 output "web_public_ip" {
   value = digitalocean_droplet.web.*.ipv4_address
 }
 # output "db_public_ip" {
 #   value = digitalocean_droplet.db.*.ipv4_address
+# }
+# output "bastion_private_ip" {
+#   value = digitalocean_droplet.bastion.ipv4_address_private
 # }
