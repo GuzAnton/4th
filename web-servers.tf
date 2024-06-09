9e62fb59c79bb00eb6986e72f3bea965b03bb64a
Tony <guzantony@gmail.com>
2024-06-09 12:05:45 +0300

    fixed zone_id

--- web-servers.tf
+++ web-servers.tf
@@ -220,7 +220,7 @@ resource "digitalocean_certificate" "cert" {
   certificate_chain = file("/etc/letsencrypt/live/fourthestate.app/fullchain.pem")
 }
 resource "cloudflare_record" "project_subdomain" {
-  zone_id = data.cloudflare_zones.fourthestate_app.zone_id
+  zone_id = data.cloudflare_zones.fourthestate_app.id
   name    = var.subdomain
   value   = element(digitalocean_droplet.web.*.ipv4_address, 0)
   type    = "A"
