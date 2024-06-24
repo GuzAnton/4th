ea7c5c5558f67242e946c8ffa9dbef2690a8e6ef
Tony <guzantony@gmail.com>
2024-06-24 15:11:21 +0300

    test script

--- Ansible/grafana.sh
+++ Ansible/grafana.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GRAFANA_VERSION="10.4.4"
-PROMETHEUS_URL="http://localhost"
+# PROMETHEUS_URL="http://localhost"
 
 sudo apt-get install -y apt-transport-https software-properties-common wget
 sudo mkdir -p /etc/apt/keyrings/
@@ -10,18 +10,19 @@ echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stab
 
 # Updates the list of available packages
 sudo apt-get update
-sudo apt-get install -y adduser libfontconfig1 musl
+sudo apt-get install -y 
+adduser libfontconfig1 musl
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_${GRAFANA_VERSION}_amd64.deb
 sudo dpkg -i grafana-enterprise_${GRAFANA_VERSION}_amd64.deb
 echo "export PATH=/usr/share/grafana/bin:$PATH" >> /etc/profile
 
-cat <<EOF> /etc/grafana/provisioning/datasources/prometheus.yaml
-api.version: 1
-datasources:
-    - name: Prometheus
-      type: Prometheus
-      url: ${PROMETHEUS_URL}
-EOF
+# cat <<EOF> /etc/grafana/provisioning/datasources/prometheus.yaml
+# api.version: 1
+# datasources:
+#     - name: Prometheus
+#       type: Prometheus
+#       url: ${PROMETHEUS_URL}
+# EOF
 
 systemctl daemon-reload
 systemctl start grafana-server
