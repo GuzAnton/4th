ebc99944bdc2021413f0d7ce104bdefacedcd721
Tony <guzantony@gmail.com>
2024-06-27 14:35:54 +0300

    updated loadbalancer

--- web-servers.tf
+++ web-servers.tf
@@ -55,18 +55,18 @@ resource "digitalocean_loadbalancer" "web" {
   forwarding_rule {
     entry_port       = 443
     entry_protocol   = "https"
-    target_port      = 80
-    target_protocol  = "http"
+    target_port      = 443
+    target_protocol  = "https"
     certificate_name = digitalocean_certificate.cert.name
   }
 
-  forwarding_rule {
-    entry_port = 80
-    entry_protocol = "http"
-    target_port = 80
-    target_protocol = "http"
-    certificate_name = digitalocean_certificate.cert.name
-  }
+  # forwarding_rule {
+  #   entry_port = 80
+  #   entry_protocol = "http"
+  #   target_port = 80
+  #   target_protocol = "http"
+  #   certificate_name = digitalocean_certificate.cert.name
+  # }
   
   healthcheck {
     port                     = 80
