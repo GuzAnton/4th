f6c942e8ce433d936c17bfcf633ef260320ec2a7
Tony <guzantony@gmail.com>
2024-07-08 15:13:39 +0300

    test new provider

--- providers.tf
+++ providers.tf
@@ -13,7 +13,7 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
+      source  = "digitalocean.digitalocean"
       version = ">= 2.36"
     }
     cloudflare = {
