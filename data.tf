b86699e81db74837eb0b24027b1c401d2c7c16c5
Tony <guzantony@gmail.com>
2024-07-16 21:14:33 +0300

    test

--- data.tf
+++ data.tf
@@ -25,3 +25,6 @@ data "cloudflare_zones" "fourthestate_app" {
 data "digitalocean_ssh_key" "default" {
   name = "autodeploy_key"
 }
+data "digitalocean_certificate" "cert"{
+  name = "autodeploy-certificate"
+}
