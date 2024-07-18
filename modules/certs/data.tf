f5de28030d46d195c86da242a032b7b07074973f
Tony <guzantony@gmail.com>
2024-07-18 10:14:50 +0300

    added data.tf

--- modules/certs/data.tf
+++ modules/certs/data.tf
@@ -1,3 +1,7 @@
 # data "digitalocean_certificate" "cert" {
 #   name = "fourthestate-app-cert"
 # }
+data "digitalocean_certificate" "cert" {
+  count = var.create_cert ? 0 : 1
+  name  = var.cert_name
+}
