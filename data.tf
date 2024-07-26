6460a575c0f72b80ebdacb130923c9eaf9a78c5c
Tony <guzantony@gmail.com>
2024-07-26 09:29:49 +0300

    cleaned

--- data.tf
+++ data.tf
@@ -13,9 +13,6 @@ ${join("\n", digitalocean_droplet.db.*.ipv4_address_private)}
 bastion ansible_host=${digitalocean_droplet.bastion.ipv4_address_private} ansible_user=root ansible_ssh_private_key_file=~/.ssh/id_rsa
   TEMPLATE
 }
-#when working with load balancer we have to add this in TENPLATE section
-#[load_balancers]
-# ${join("\n", [digitalocean_loadbalancer.web.ip])}
 
 data "cloudflare_zones" "fourthestate_app" {
   filter {
--- project2.tfvars
+++
@@ -1,6 +0,0 @@
-vpc_range = "172.20.2.0/24"
-vpc_name = "project2"
-subdomain = "test2"
-FireWall_Name_Web = "test2-web-fw"
-FireWall_Name_DB = "test2-db-fw"
-Bastion_firewall_name = "test2-bastion-fw"
