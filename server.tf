3ccb4d3ce34213142cae0ec3ffc608457aa83f10
Tony <guzantony@gmail.com>
2024-08-01 23:08:28 +0300

    integration with spaces

--- server.tf
+++ server.tf
@@ -79,3 +79,16 @@ resource "cloudflare_record" "project_subdomain" {
   type    = "A"
   ttl     = 300
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "fe-autodeploy-01"  
+    key            = "terraform-backend/${var.subdomain}/terraform.tfstate"
+    region         = "us-east-1"  
+    endpoint       = "https://fra1.digitaloceanspaces.com"  
+    access_key     = var.do_spaces_access_key
+    secret_key     = var.do_spaces_secret_key
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
