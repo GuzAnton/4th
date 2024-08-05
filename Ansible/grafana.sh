27164f0f5f915832629804b84ee2bf635d4156f8
Tony <guzantony@gmail.com>
2024-08-05 12:19:55 +0300

    added grafana

---
+++ Ansible/grafana.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install -y apt-transport-https software-properties-common wget
+sudo mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+# Updates the list of available packages
+sudo apt-get update
+sudo apt-get install -y adduser libfontconfig1 musl
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_11.0.0_amd64.deb
+sudo dpkg -i grafana-enterprise_11.0.0_amd64.deb
+systemctl daemon-reload
+systemctl start grafana-server
+systemctl enable grafana-server
