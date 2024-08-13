e9d622a9c9a8cf5faa499d69622133895c50cb0c
Tony <guzantony@gmail.com>
2024-08-13 10:15:11 +0300

    providers updated

--- providers.tf
+++ providers.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.36"
+      version = "2.36"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      version = "4.39.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.67"
     }
   }
 }
