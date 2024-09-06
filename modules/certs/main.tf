b794d4e973713ccbb38c83935cce349caf62b0a5
Tony <guzantony@gmail.com>
2024-09-06 10:57:03 +0300

    cleared code

--- modules/certs/main.tf
+++ modules/certs/main.tf
@@ -1,9 +1,3 @@
-# resource "digitalocean_certificate" "cert" {
-#   name              = "autodeploy-certificate"
-#   private_key       = file("/etc/letsencrypt/live/fourthestate.app/privkey.pem")
-#   leaf_certificate  = file("/etc/letsencrypt/live/fourthestate.app/cert.pem")
-#   certificate_chain = file("/etc/letsencrypt/live/fourthestate.app/fullchain.pem")
-# }
 resource "digitalocean_certificate" "cert" {
   count             = var.create_cert ? 1 : 0
   name              = var.cert_name
