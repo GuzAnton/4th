e301107aa719188c31f98f1e713b7861814bf9e3
Tony <guzantony@gmail.com>
2024-10-07 14:51:49 +0300

    fixed size of the cluster

--- vars.tf
+++ vars.tf
@@ -37,7 +37,7 @@ variable "web_droplet_size" {
   default = "s-2vcpu-2gb"
 }
 variable "cluster_size" {
-  default = "db-s-2vcpu-2gb"
+  default = "db-s-2vcpu-4gb"
 }
 variable "db_droplet_size" {
   default = "s-2vcpu-2gb"
