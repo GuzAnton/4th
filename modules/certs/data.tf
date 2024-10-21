e871622939842fe5763de602f645ff4c5584ac52
Tony <guzantony@gmail.com>
2024-10-21 23:57:11 +0300

    test updated cert creation code

--- modules/certs/data.tf
+++ modules/certs/data.tf
@@ -1,4 +1,9 @@
-data "digitalocean_certificate" "certificate" {
-  count = var.create_cert ? 0 : 1
-  name  = var.cert_name
+# data "digitalocean_certificate" "certificate" {
+#   count = var.create_cert ? 0 : 1
+#   name  = var.cert_name
+# }
+
+# Data source to check if the certificate already exists
+data "digitalocean_certificate" "existing" {
+  name = var.cert_name
 }
